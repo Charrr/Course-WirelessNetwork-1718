267c8832b8f0827e65917d5429b5386c2de68e5b
Charrr <906355844@qq.com>
2017-12-11 19:04:26 +0800

    mSeq.m completed

---
+++ exp2/init.m
@@ -0,0 +1,3 @@
+poly = [0,5,7,8,9,13,15];
+M = mSeq(poly);
+stem(M(1:100)); title('M-Sequence(1:100)');
--- exp2/mSeq.m
+++ exp2/mSeq.m
@@ -4,13 +4,23 @@ function [ M ] = mSeq( orders )
 %           e.g., 1+x^2+x^5 corresponds to [0,2,5]
 
 % Convert the polynomial orders into a binary sequence representing the connections of registers.
-len = max(orders)+1;
+len = max(orders)+1;    % Total length of the LFSR.
 seq = zeros( 1, len );
 for i = 1:length(orders)
     seq(orders(i)+1) = 1;
 end
 
-M = seq;
+N = 2^len - 1;  % The period of the M-sequence.
+M = zeros(1,N); % Initialising M, of length 2^n-1
+
+register = [1, zeros(1,len-1)];
+M(1) = register(1);
+for i = 2:N
+    nextRegister(1:len-1) = register(2:len);
+    nextRegister(len) = mod(sum(seq.*register), 2);
+    register = nextRegister;
+    M(i) = register(1);
+end
 
 end
 
