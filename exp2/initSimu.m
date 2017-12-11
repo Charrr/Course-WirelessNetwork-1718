0427edbb7bfbe6c81c52e578d985209fe76a608d
Charrr <906355844@qq.com>
2017-12-12 02:11:11 +0800

    minimal perfections

--- exp2/initSimu.m
+++ exp2/initSimu.m
@@ -5,7 +5,7 @@ fc = 128; % carrier frequency 128Hz
 
 M = mSeq([0,5,7,8,9,13,15]);    % Generate the m-sequence.
 
-mt = [1 0 1 0 1 1 1 0 1 1 0 0 1 0 0 1 0 1];   % An arbitrary signal m(t).
+mt = [1 0 1 0 1 1 1 0 1 1 0 0 1 0 0 0 0 1 1 0];   % An arbitrary signal m(t).
 mt_in = kron(mt, ones(1,gain)); % Interpolating the signal before multiplying by the pn code.
 figure, plot(mt_in); title('m(t)');
 
