9373cd862a3e34632cca39071c2054893065cd2b
Charrr <906355844@qq.com>
2017-12-31 20:20:14 +0800

    exp3 updated

---
+++ exp3/erlangB.m
@@ -0,0 +1,7 @@
+function [ PB ] = erlangB(m, A)
+if m == 0
+    PB = 1;   % Until m decreases to 0, P(O,A)=1
+else
+    PB = 1/(1 + m/( A*erlangB(m-1, A) )) ; % Recursion takes place here. m decreases by 1 every loop until 0. 
+end
+end
